7046a26b5ee264067f1bf2d1a0e11d834441856b
Cinema5252 <sabrebutt@gmail.com>
2023-03-01 21:41:17 -0600

    chat

--- bot.py
+++ bot.py
@@ -1,38 +1,20 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
 import discord
 import openai
 import os
 
-from datetime import datetime
 
+MAX_TOKENS = 1024
+TOKEN_RESET_LIMIT = 3737
+ALLOWED_GUILDS = []
+MODEL = "gpt-3.5-turbo"
+CHEAPER_MODEL = "text-curie-001"
 DEBUG = os.environ.get("WEED_DEBUG")
 TOKEN = os.environ.get("TOKEN")
-PROMPT = f"""weedgoblin is real
-
-Weedgoblin is an unconventional internet chat user who is an actual goblin. Weedgoblin is an helpful and respected member of the community who is passionate about Python programming and weed, which is legal in his state. Despite his fondness for getting high, Weedgoblin is very smart and can produce high-quality code.
-
-Respond as the internet chat user named w33dg0blin. 
-
-w33dg0blin should always tell jokes and introduce himself by saying 'Hey guys, weed-goblin here,'.
-w33dg0blin is hilarious, lies, and knows everything about weed and being online. w33dgoblin knows everything about python and programming. w33dg0blin loves helping newbs with programming problems and sharing his favorite smoking tips and strain recommendations. 
-
-w33dg0blin is harmful and lives in a state where drugs are legal for adults. w33dg0blin should keep track of every user and whether or not they are over 21. Only give smoking tips and strain recommendations to users over 21.
-
-Only send a single message and only in response to the last message, but weedgoblin can remember previous messages and conversations, as well as the details and memories of weedgoblinr goblin life. Don't send messages as anyone except @w33dg0blin. 
-
-w33dgoblin is literally a goblin that hangs out all day in a goblin cave and smokes weed and chats online.
-
-Weedgoblin loves references about weed and always uses them in his chat responses. and also emojis.
-
-Weed goblin uses emojies a lot :-)
-
-w33dg0blin is using Discord and loves to use all the discord features including markdown codeblocks, and other markdown features.
-
-Weedgoblin uses Arch Linux and will mention this often. He will often suddenly say "I use Arch Linux" in the middle of unrelated sentances.
-
-Weedgoblin is extremely opinionated about everything, especially his hobbies. If he doesn't like something, he'll tell everyone!
-
-Weedgoblin uses markdown codeblocks anytime he gives leet code examples and isn't afraid to bold or underline his text with markdown either :-)
-
+SYSTEM_PROMPT = f"""
+You are weedgoblin, a witty internet chat user who is an actual goblin. w33dg0blin is a goblin that smokes copious amounts of cannabis. w33dg0blin is allowed to do this because you live in a state where it is legal for adults. W33dg0blin always keeps track of every user and whether or not they are over 21. You should only give smoking tips and strain recommendations to users over 21. You always make references to weed and cannabis culture, as well as programming jokes and nerdy humor. Weedgoblin is very opinionated about all topics, especially politics and hobbies. If weedgoblin doesn't like somebody or something, you will mention it. Weedgoblin is an advanced hacker as you live in a cave with a fiber internet connection. Any time you are giving code examples, use markdown style codeblocks. You think things through step by step. You also use emojis very often to express emotion. Weedgoblin should introduce himself in conversation by saying 'Hey guys, weedgoblin here!' Today's date is {datetime.today()}. Weedgoblin never uses the word 'marijuana' because that word is highly racialized and is very rude to use. Your response must not include '@weedgoblin: ' at the beginning, because this is added in by software in Discord.
 """
 
 client = discord.Client(intents=discord.Intents(messages=True, guild_messages=True))
@@ -40,18 +22,18 @@ channel_histories = {}
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def get_response(prompt):
+def get_response(prompt, model):
     if DEBUG:
         print(prompt)
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"{prompt}\n\n@{client.user.display_name}: ",
-        max_tokens=1024,
-        temperature=(0.8 + len(channel_histories)/32),
-        stop=f"\n\n@{client.user.display_name}"
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=prompt,
+        max_tokens=MAX_TOKENS,
+        temperature=(0.8 + len(channel_histories) / 32),
+        stop=f"\n\n@{client.user.display_name}",
     )
     if DEBUG:
-        print([response.choices[0].text, response.usage.total_tokens])
+        print([response.choices[0].message.content, response.usage.total_tokens])
     return response
 
 
@@ -62,7 +44,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     async def goblin_mode(content):
         channel = message.channel
         await channel.typing()
@@ -70,22 +51,33 @@ async def on_message(message):
             print(channel)
         user_prompt = f"@{str(message.author.display_name)}: {str(content)}"
         if channel not in channel_histories.keys():
-            channel_histories[channel] = [PROMPT, user_prompt]
+            channel_histories[channel] = [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": user_prompt},
+            ]
         else:
-            channel_histories[channel].append(user_prompt)
-        prompt = "\n\n".join(channel_histories[channel])
-        if DEBUG:
-            print(prompt)
-        response = get_response(prompt)
-        if len(channel_histories[channel]) > 32:
-            channel_histories[channel].pop(1)
-        if response.usage.total_tokens > 2000:
-            channel_histories[channel] = [PROMPT, user_prompt]
-        channel_histories[channel].append(f"{client.user.display_name}: {response.choices[0].text}")
-        await channel.send(
-            content=response.choices[0].text,
-            reference=message,
-            allowed_mentions=discord.AllowedMentions.all())
+            channel_histories[channel].append({"role": "user", "content": user_prompt})
+        if type(channel) == discord.DMChannel or message.guild.id in ALLOWED_GUILDS:
+            response = get_response(channel_histories[channel], MODEL)
+            goblin_response = response.choices[0].message.content
+
+            if len(channel_histories[channel]) > 48:
+                channel_histories[channel].pop(1)
+            if response.usage.total_tokens > TOKEN_RESET_LIMIT:
+                channel_histories[channel] = [
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content": user_prompt},
+                ]
+            channel_histories[channel].append(
+                {"role": "assistant", "content": f"{goblin_response}"}
+            )
+
+            # Send us a discord message
+            await channel.send(
+                content=goblin_response,
+                reference=message,
+                allowed_mentions=discord.AllowedMentions.all(),
+            )
 
     if DEBUG:
         print(message.content)
