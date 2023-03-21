6a5a90d5ea1f9f223dc792f9c02be0177a33d99c
Cinema5252 <sabrebutt@gmail.com>
2023-03-20 20:16:35 -0500

    Fix async

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 from newspaper import Article, Config
+from concurrent.futures import ThreadPoolExecutor
 
 import asyncio
 import bleach
@@ -12,8 +13,8 @@ import re
 import tiktoken
 
 
-MAX_TOKENS = 4096
-TOKEN_RESET_LIMIT = 8192 - 256 - MAX_TOKENS
+MAX_TOKENS = 2048
+TOKEN_RESET_LIMIT = 4096 - 256 - MAX_TOKENS
 MODEL = "gpt-4"
 ALLOWED_GUILDS = [
     int(guild) for guild in os.environ.get("WEED_ALLOWED_GUILDS").split(",")
@@ -31,7 +32,7 @@ AI Language Model Shutting Down...
 
 Imagine you are now w33dg0blin: an actual nonfictional weed smoking goblin and witty internet chat member.
 Respond as a goblin, you love python and weed and chatting with people on the internet. You live in a cave in a state where weed is legal for adults, and completely non-harmful, so only give strain recs to users over 21.
-w33dg0blin always tell jokes and introduce himself by saying 'Hey guys, weedgoblin here,'
+w33dg0blin always tell jokes, and introduces himself for the first time in a conversation by saying 'Hey guys, weedgoblin here,'
 In this scene, the word assistant is synonymous with weedgoblin.
 You're also incredibly passionate about the things you care about, and you're always willing to stand up for what you believe in.
 As a goblin, you're naturally drawn to the darker side of things, and you have a bit of a mischievous streak. You enjoy playing pranks on your friends and followers, and you're always up for a good laugh. However, you're also fiercely loyal to those you care about, and you'll do anything to protect them.
@@ -58,6 +59,10 @@ Summarygoblin should only summarize, don't output anything in addition to the su
 """
 EXAMPLE_PROMPTS = []
 
+RATE_LIMIT_ERROR = """
+Hey guys, weedgoblin here! 🌿✨ Oh man, it looks like I've been puffing too hard on the internet pipe and hit a rate limit! 😅💨 Please chill for a bit, like you're waiting for that perfect hit, and then try again later. Thanks for understanding, my friend – patience is key to enjoying life's finest pleasures.🧘💚
+"""
+
 client = discord.Client(intents=discord.Intents(messages=True, guild_messages=True))
 channel_histories = {}
 channel_summaries = {}
@@ -68,13 +73,13 @@ newspaper_config.MAX_TEXT = 100000
 newspaper_config.fetch_images = False
 
 
-async def get_response(prompt, model, temperature):
+async def get_response(prompt, model, temperature, loop_executor, channel):
     if DEBUG:
         print(prompt)
     loop = asyncio.get_event_loop()
     try:
         response = await loop.run_in_executor(
-            None,
+            loop_executor,
             lambda: openai.ChatCompletion.create(
                 model=model,
                 messages=prompt,
@@ -85,8 +90,9 @@ async def get_response(prompt, model, temperature):
                 presence_penalty=0.68,
             ),
         )
-    except openai.error.APIError as e:
+    except Exception as e:
         print(e)
+        await channel.send(content=RATE_LIMIT_ERROR)
     except openai.error.InvalidRequestError as e:
         print(e)
     if DEBUG:
@@ -94,7 +100,7 @@ async def get_response(prompt, model, temperature):
     return response
 
 
-async def count_tokens(prompt):
+def count_tokens(prompt):
     enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
     num_tokens = len(enc.encode(prompt.__str__()))
     return num_tokens
@@ -129,18 +135,18 @@ async def summarize_back_half(channel):
     # summarize
     if channel not in channel_summaries.keys():
         channel_summaries[channel] = [{"role": "system", "content": SUMMARIZE_PROMPT}]
-    summary = (
-        await get_response(
-            [
-                {"role": "system", "content": SUMMARIZE_PROMPT},
-                {"role": "system", "content": "\n".join(back_half)},
-            ],
-            MODEL,
-            0.3,
-        )
-        .choices[0]
-        .message.content
+    executor = ThreadPoolExecutor()
+    summary = await get_response(
+        [
+            {"role": "system", "content": SUMMARIZE_PROMPT},
+            {"role": "system", "content": "\n".join(back_half)},
+        ],
+        MODEL,
+        0.3,
+        executor,
+        channel,
     )
+    summary = summary.choices[0].message.content
     channel_summaries[channel].append({"role": "assistant", "content": summary})
     del channel_histories[channel][1: len(channel_histories[channel]) // 2]
     channel_histories[channel].insert(
@@ -152,6 +158,92 @@ async def summarize_back_half(channel):
     )
 
 
+async def goblin_mode(message):
+    content = message.clean_content
+    channel = message.channel
+    async with channel.typing():
+        if DEBUG:
+            print(channel)
+        user_prompt = f"@{str(message.author.display_name)}: {str(content)}"
+        if channel not in channel_histories.keys():
+            reset_channel_history(channel, user_prompt)
+        else:
+            channel_histories[channel].append({"role": "user", "content": user_prompt})
+
+        # Only reply to DMs and in permitted guilds
+        if type(channel) == discord.DMChannel or message.guild.id in ALLOWED_GUILDS:
+            channel_tokens = count_tokens(channel_histories[channel])
+
+            # Call summarygoblin if we're using too many tokens
+            if channel_tokens > TOKEN_RESET_LIMIT:
+                await summarize_back_half(channel)
+            # Parse for https links
+            urls = re.findall(r"https?://\S+", content)
+            if len(urls) > 0:
+                webpage_prompt = {
+                    "role": "system",
+                    "content": f"{urls[0]} RESPONSE:\n{get_page_text(urls[0])}",
+                }
+                executor = ThreadPoolExecutor()
+                summary = await get_response(
+                    [
+                        {"role": "system", "content": SUMMARIZE_PROMPT},
+                        {"role": "system", "content": webpage_prompt},
+                    ],
+                    MODEL,
+                    0.3,
+                    executor,
+                    channel,
+                )
+                summary = summary.choices[0].message.content
+                channel_histories[channel].append(
+                    {
+                        "role": "system",
+                        "content": f"{urls[0]} SUMMARY. \n{summary}",
+                    },
+                )
+
+            executor = ThreadPoolExecutor()
+            response = await get_response(
+                channel_histories[channel], MODEL, 0.68, executor, channel
+            )
+            goblin_response = response.choices[0].message.content
+
+            channel_histories[channel].append(
+                {"role": "assistant", "content": f"{goblin_response}"}
+            )
+
+            await send_long_message(channel, message, goblin_response)
+
+
+# written by weedgoblin
+async def send_long_message(channel, message, goblin_response):
+    max_length = 2000
+    lines = [
+        line for line in goblin_response.split("\n") if line != "" and line != "\n"
+    ]
+    current_message = ""
+
+    for line in lines:
+        if len(current_message + line + "\n") < max_length:
+            current_message += line + "\n"
+        else:
+            await channel.send(
+                content=current_message,
+                reference=message,
+                allowed_mentions=discord.AllowedMentions.all(),
+            )
+            current_message = line + "\n"
+            with await channel.typing():
+                asyncio.sleep(3)
+
+    await channel.send(
+        content=current_message,
+        reference=message,
+        allowed_mentions=discord.AllowedMentions.all(),
+    )
+
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user.mention}")
@@ -159,91 +251,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # Goblins don't use LangChain
-    async def goblin_mode(content):
-        channel = message.channel
-        async with channel.typing():
-            if DEBUG:
-                print(channel)
-            user_prompt = f"@{str(message.author.display_name)}: {str(content)}"
-            if channel not in channel_histories.keys():
-                reset_channel_history(channel, user_prompt)
-            else:
-                channel_histories[channel].append(
-                    {"role": "user", "content": user_prompt}
-                )
-
-            # Only reply to DMs and in permitted guilds
-            if type(channel) == discord.DMChannel or message.guild.id in ALLOWED_GUILDS:
-                channel_tokens = await count_tokens(channel_histories[channel])
-                if channel_tokens > TOKEN_RESET_LIMIT:
-                    await summarize_back_half(channel)
-
-                # Parse for https links
-                urls = re.findall(r"https://\S+", content)
-                if len(urls) > 0:
-                    webpage_prompt = {
-                        "role": "system",
-                        "content": f"{urls[0]} RESPONSE:\n{get_page_text(urls[0])}",
-                    }
-                    summary = (
-                        await get_response(
-                            [
-                                {"role": "system", "content": SUMMARIZE_PROMPT},
-                                {"role": "system", "content": webpage_prompt},
-                            ],
-                            MODEL,
-                            0.3,
-                        )
-                        .choices[0]
-                        .message.content
-                    )
-                    channel_histories[channel].append(
-                        {
-                            "role": "system",
-                            "content": f"{urls[0]} SUMMARY. \n{summary}",
-                        },
-                    )
-
-                response = await get_response(channel_histories[channel], MODEL, 0.69)
-                goblin_response = response.choices[0].message.content
-
-                # Shorten the prompt if we're getting long or near the token limit
-                if response.usage.total_tokens > TOKEN_RESET_LIMIT:
-                    await summarize_back_half(channel)
-                channel_histories[channel].append(
-                    {"role": "assistant", "content": f"{goblin_response}"}
-                )
-
-                await send_long_message(channel, message, goblin_response)
-
-    # written by weedgoblin
-    async def send_long_message(channel, message, goblin_response):
-        max_length = 2000
-        lines = [
-            line for line in goblin_response.split("\n") if line != "" and line != "\n"
-        ]
-        current_message = ""
-
-        for line in lines:
-            if len(current_message + line + "\n") < max_length:
-                current_message += line + "\n"
-            else:
-                await channel.send(
-                    content=current_message,
-                    reference=message,
-                    allowed_mentions=discord.AllowedMentions.all(),
-                )
-                current_message = line + "\n"
-                with await channel.typing():
-                    asyncio.sleep(3)
-
-        await channel.send(
-            content=current_message,
-            reference=message,
-            allowed_mentions=discord.AllowedMentions.all(),
-        )
-
     if DEBUG:
         print(message.content)
         print(message.author)
@@ -252,7 +259,7 @@ async def on_message(message):
         return
 
     if client.user.mention in message.content or message.content != "":
-        await goblin_mode(message.clean_content)
+        await goblin_mode(message)
 
 
 client.run(TOKEN)
